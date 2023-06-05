aabaf3e6e72e9a09a9ab338a4f1e41998851ce08
Maria L <maria_lich@mail.ru>
2023-06-05 23:06:04 +0300

    tasks finalised

--- Task_3/Program.cs
+++ Task_3/Program.cs
@@ -4,12 +4,15 @@
 // 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 // 6, 1, 33 -> [6, 1, 33]
 
+int[] GetArr() {
 int [] arr = new int [8];
 Random randNumArr = new Random();
 
 for (int i = 0; i < arr.Length; i++)
 {
-   arr[i] = randNumArr.Next(-15, 100);
+   arr[i] = randNumArr.Next(int.MinValue, int.MaxValue);
+}
+return arr;
 }
 
-Console.WriteLine($"[{String.Join(", ", arr)}]");
+Console.WriteLine($"[{String.Join(", ", GetArr())}]");
